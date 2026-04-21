3e48a5ec66c261cb6463b46dc0ff2d61c2634fb6
Cyan512 <182935@unsaac.edu.pe>
2026-04-21 08:34:14 -0500

    image

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,10 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const STRAPI_PROTOCOL = (process.env.NEXT_PUBLIC_STRAPI_PROTOCOL || 'http') as
-  | 'http'
-  | 'https';
-const STRAPI_HOST = process.env.NEXT_PUBLIC_STRAPI_HOST || 'localhost';
-const STRAPI_PORT = process.env.NEXT_PUBLIC_STRAPI_PORT || '1337';
-
 const nextConfig: NextConfig = {
   images: {
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
-      {
-        protocol: STRAPI_PROTOCOL,
-        hostname: STRAPI_HOST,
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cms-aw1k.onrender.com',
-        pathname: '/uploads/**',
-      },
       {
         protocol: 'https',
         hostname: 'strapi.hostal-korikallpa.com',
