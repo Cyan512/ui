862c69aeab5cb8552173d03228bfa545433bb3a2
Cyan512 <182935@unsaac.edu.pe>
2026-04-21 08:51:11 -0500

    image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
       {
